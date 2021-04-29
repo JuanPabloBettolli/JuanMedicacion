12796b915b9f60661d181fac56936db17c1914fc
JuanPabloBettolli <73622205+JuanPabloBettolli@users.noreply.github.com>
2021-04-29 20:23:31 -0300

    Add files via upload

---
+++ 2-variables.js
@@ -0,0 +1,85 @@
+//aca console log
+//console.log("Hi")
+
+
+//console.log("Hi")
+
+//console.info("info")
+
+//console.table(['one','two','three'])
+//console.count()
+//console.count()
+
+//VARIABLES
+
+//let numero = 10
+//console.log(10)
+//console.log(numero)
+
+/*Concatena palabras
+const myFirstName = 'JUAN'
+const mySecondName = 'Pablo'
+const myLastName = 'Bettolli'
+
+console.log(myFirstName, mySecondName, myLastName)*/
+/*const number = 42
+alert ("HOLA COMO ESTAS, tengo: "+ number + " años")*/
+
+//promt
+/*let name= prompt("Cual es tu nombre?")
+
+console.log(name)
+
+alert(name)*/
+
+//confirm
+/*let result = confirm('Estas al frente de una pc?')
+console.log(result)*/
+
+/*let nombre = prompt("Ingrese su nombre: ")
+let edad = prompt("Ingrese su edad: ")
+
+//console.log(edad)
+
+alert ("Me llamo: " + nombre + " El doble de mi edad es:  "+edad*2)
+console.log(nombre+edad)*/
+
+/*let value = true
+console.log(typeof value)
+
+console.log(value)
+console.log(String(value))*/
+
+/*let num = '31'
+let newNum = Number(num)
+
+console.log("HOLA")
+console.log(num)
+console.log(newNum)*/
+
+console.log(1);
+console.log(Boolean("HOLA"))
+console.log(Boolean(1))
+console.log(Boolean({}))
+
+//VARiables Booleanas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
